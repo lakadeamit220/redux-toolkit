16b81a40b273030ef2bf509c5441ff1b835cb5ba
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-13 12:37:40 +0530

    Added removeTodo functionality.

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
 export default function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
   return (
     <>
       <h1>Todos</h1>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>Delete</button>
+        </li>
+      ))}
     </>
   );
 }
