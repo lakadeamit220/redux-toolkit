7ce5c9498b9c8d01647198c25c277d9e0f7fbe11
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-13 12:01:30 +0530

    Added form to add todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import AddTodo from "./components/AddTodo";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-5xl font-bold text-green-700">
         Redux-Toolkit
       </h1>
+      <AddTodo/>
     </>
   );
 }
